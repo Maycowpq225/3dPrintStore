ae57136a68150e940995b2445da0bb352ce0cfe1
Maycowpq225 <maycowfellipo@hotmail.com>
2024-06-29 13:05:24 -0300

    First commit

---
+++ src/main/java/maycow/WorkOutHelperAPI/WorkOutHelperApiApplication.java
@@ -0,0 +1,13 @@
+package maycow.WorkOutHelperAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkOutHelperApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkOutHelperApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/maycow/WorkOutHelperAPI/WorkOutHelperApiApplicationTests.java
@@ -0,0 +1,13 @@
+package maycow.WorkOutHelperAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkOutHelperApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
